4d2160b5d638245489907dba05427c5d4eb0e52a
n.dumont <n.dumont@anasta.eu>
2013-06-02 16:43:01 +0200

    ne réagi plus a 'ouvertur de BM mais a l'expiration ou supression du cookie de session hps en le recréant

--- BmNotifier/chrome/BMNotifier/background.js
+++ BmNotifier/chrome/BMNotifier/background.js
@@ -1,60 +1,100 @@
 var host; 
-var hps; 
-
+var Hostknow=false;
 
 chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.create({'url': "https://"+host});
+	if (!Hostknow){
+		findHost(false);
+	}
+	if (Hostknow) {
+		chrome.cookies.get({"url":getBmUrl(),"name":"BMHPS"},cookiesBMHPS);
+		chrome.tabs.create({'url': getBmUrl()});
+	}
 });
 
-function checkForBMUrl(tabId, changeInfo, tab) {
-  // If the letter 'g' is found in the tab's URL...
-  if (tab.url.indexOf(host) > -1) {
-    console.info("open bluemind");
-	chrome.cookies.get({"url":tab.url,"name":"BMHPS"},cookiesBMHPS);
-  }
-};
+function getBmUrl() {
+  return "https://"+host;
+}
 
 function cookiesBMHPS(cookie){
-	if (cookie==null){
-		console.info("BM NOT LOGGED");
+	console.debug(cookie);
+	if (cookie==null || cookie.value==""){
+	// session expired or not created make new cookies
+		console.debug("Cookies BM not present");
+		setCookiesBMHPS(getHPS());
 	}else{
-		console.info("BM LOGGED");
-		newHps = getHPS();
-		setCookiesBMHPS(newHps);
-	}
-}
-function setCookiesBMHPS(hps, url){
-	chrome.cookies.set({"url":"https://"+host,"name":"BMHPS","value":hps,"path":"/"});	
+		// already a cookies 
+		//
+		console.debug("cookies BM present");
+	}	
 }
 
+function setCookiesBMHPS(hps){
+	chrome.cookies.set({"url":getBmUrl(),"name":"BMHPS","value":hps,"path":"/","domain":host},function(cookie) {console.debug(cookie)});	
+}
 
 function getHPS(){
-xhr.open("GET", "http://localhost:51985/hps", false);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // JSON.parse does not evaluate the attacker's scripts.
-    var resp = JSON.parse(xhr.responseText);
-	hps=resp.HPS;
-	console.info(hps);
+	var hps;
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "http://localhost:51985/hps", false);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status>=200 & xhr.status<300) {
+			// JSON.parse does not evaluate the attacker's scripts.
+			var resp = JSON.parse(xhr.responseText);
+			hps=resp.HPS;
+			console.debug(hps);
+			
+		}
+	};
+	xhr.onerror= function() {
+		Hostknow=false;
+		updateIcon();
+	};
 	
-  }
-}
-xhr.send();
-return hps;
+	xhr.send();
+	return hps;
 }
 
-// Listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(checkForBMUrl);
-	
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:51985/host", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // JSON.parse does not evaluate the attacker's scripts.
-    var resp = JSON.parse(xhr.responseText);
-	host=resp.HOST;
-	console.info(host);
-	
+function updateIcon() {
+  if (Hostknow) {
+    chrome.browserAction.setIcon({path:"bm.png"});
+    //chrome.browserAction.setBadgeText({text:"Go to "+ host});
+  } else {
+    chrome.browserAction.setIcon({path: "bmwarning.png"});
+    //chrome.browserAction.setBadgeText({  text: "host unknow"  });
   }
 }
-xhr.send();
+
+function findHost(async){	
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "http://localhost:51985/host", async);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status>=200 & xhr.status<300) {
+			// JSON.parse does not evaluate the attacker's scripts.
+			var resp = JSON.parse(xhr.responseText);
+			host=resp.HOST;
+			console.debug(host);
+			Hostknow=true;
+			updateIcon();
+		}
+	};
+	xhr.onerror= function() {
+		Hostknow=false;
+		updateIcon();
+	};
+
+	xhr.send();
+}
+updateIcon();
+findHost(true);
+chrome.cookies.get({"url":getBmUrl(),"name":"BMHPS"},cookiesBMHPS);
+chrome.cookies.onChanged.addListener(function(changeInfo) {
+console.debug(changeInfo.cookie);
+if (changeInfo.removed && changeInfo.cookie.name=="BMHPS"){
+	if (!Hostknow){
+		findHost(false);
+	}
+	if (Hostknow) {
+		setCookiesBMHPS(getHPS());
+  }
+}
+});
